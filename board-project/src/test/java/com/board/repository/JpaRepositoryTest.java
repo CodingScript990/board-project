2f30744d594398aebae311b6ac13a61f14ae98c3
trte135 <trte135@naver.com>
2023-04-03 17:28:11 +0900

    #10 - JPA Test Write
    Basic CRUD + 연관관계 mapping과 Cascading이 잘 동작하는지 보려고 작성함.
    Test Data와 관련이 있기에 이대로는 Fail함.

---
+++ board-project/src/test/java/com/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,102 @@
+package com.board.repository;
+
+import com.board.config.JpaConfig;
+import com.board.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA Connection Test")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+		// Test Filed
+		private final ArticleRepository articleRepository;
+		private final ArticleCommentRepository articleCommentRepository;
+
+		public JpaRepositoryTest(
+				@Autowired ArticleRepository articleRepository,
+				@Autowired ArticleCommentRepository articleCommentRepository)
+		{
+			this.articleRepository = articleRepository;
+			this.articleCommentRepository = articleCommentRepository;
+		}
+
+		// Test => select
+		@DisplayName("Select Test")
+		@Test
+		void givenTestData_whenSelecting_thenWorksFine() {
+			// Given
+
+			// When => import.sql data 가 얼마나 있는지 체크
+			List<Article> articles = articleRepository.findAll();
+
+			// Then
+			assertThat(articles)
+					.isNotNull()
+					.hasSize(123);
+		}
+
+		// Test => insert
+		@DisplayName("Insert Test")
+		@Test
+		void givenTestData_whenInserting_thenWorksFine() {
+			// Given
+			// previousCount long type 으로 article table data count
+			long previousCount = articleRepository.count();
+			// When
+			Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+			// Then
+			assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+		}
+
+		// Test => Update
+		@DisplayName("Update Test")
+		@Test
+		void givenTestData_whenUpdating_thenWorksFine() {
+				// Given
+				// article table data 중 index 1 을 찾아옴
+				Article article = articleRepository.findById(1L).orElseThrow();
+				// hashtag 를 수정할 것 작업
+				String updatedHashtag = "#springboot";
+				// hashtag 수정되도록 작업
+				article.setHashtag(updatedHashtag);
+				// When
+				// article table 에 수정한 것을 db 에 article table 에 저장함
+				Article savedArticle = articleRepository.saveAndFlush(article);
+				// Then
+				// save 된 article table 이 수정잘 되었는지 수정된 hashtag 를 체크
+				assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+		}
+
+		// Test => Delete
+		@DisplayName("Delete Test")
+		@Test
+		void givenTestData_whenDeleting_thenWorksFine() {
+				// Given
+				// article table data 중 index 1 을 찾아옴
+				Article article = articleRepository.findById(1L).orElseThrow();
+				// article table data 수가 몇개인지 check
+				long previousArticleCount = articleRepository.count();
+				// article comment table data 수가 몇개인지 check
+				long previousArticleCommentCount = articleCommentRepository.count();
+				// delete 된 article comment size
+				int deletedCommentsSize = article.getArticleComments().size();
+
+				// When
+				// article table 에 index 1 의 값들을 삭제해줌[해당되는 comment data(article comments) 까지]
+				articleRepository.delete(article);
+				// Then
+				// article table 에서 1개의 index 가 지워진 후 table data 가 -1 이된 table data 값이 맞는지 체크
+				assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+				// articleComment table 이 삭제된 값과 delete 된 comments size 값이 동일한지 check
+				assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+		}
+}
