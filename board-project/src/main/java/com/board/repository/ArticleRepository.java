57eb1f944117f3725b0f4a4b57c53ccf142d921f
trte135 <trte135@naver.com>
2023-04-04 18:16:58 +0900

    #1 - Data REST 설정
    Board, Comment의 JSON API 를 자동으로 Restful 하게 만들게 끔 설정함.

--- board-project/src/main/java/com/board/repository/ArticleRepository.java
+++ board-project/src/main/java/com/board/repository/ArticleRepository.java
@@ -2,6 +2,10 @@ package com.board.repository;
 
 import com.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// ArticleRepository Interface => Inherit[JpaRepository]
+// @RepositoryRestResource => Spring Data Rest API 를 사용하기 위한 작업
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
