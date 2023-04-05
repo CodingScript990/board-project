5caebbc20be7754cc5397b95adeccffafdfb42a4
trte135 <trte135@naver.com>
2023-04-05 20:31:43 +0900

    #16 - Post 관련 View Test 작성
    어떤 경로로 View 열어줄지 Test로 명세를 뽑음.

--- board-project/src/main/java/com/board/controller/ArticleController.java
+++ board-project/src/main/java/com/board/controller/ArticleController.java
@@ -1,2 +1,11 @@
-package com.board.controller;public class ArticleController {
+package com.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// ArticleController Class => Controller, RequestMapping[/articles => path]
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+		// Handler Method part
 }
--- board-project/src/test/java/com/board/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/board/controller/ArticleControllerTest.java
@@ -1,4 +1,77 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+// ArticleControllerTest => Article
+// Test package 는 무조건 @Autowired 를 명시해줘야함!!
+// WebMvcTest => Article Controller Class [include 된 아이들만 실행!]
+@DisplayName("View Controller - Post")
+@WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
-  
+		// Field add => MVC
+		private final MockMvc mvc;
+
+		// ArticleControllerTest Constructor add => mvc
+		ArticleControllerTest(@Autowired MockMvc mvc) {
+				this.mvc = mvc;
+		}
+
+		// View Get method => Post List
+		@DisplayName("View[GET] - Post List (Board) page - Successful Calling")
+		@Test
+		public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+				// Given
+
+				// When & Then
+				mvc.perform(get("/articles"))
+								.andExpect(status().isOk())
+								.andExpect(content().contentType(MediaType.TEXT_HTML))
+								.andExpect(model().attributeExists("articles"));
+		}
+
+		// View Get method => Post Detail
+		@DisplayName("View[GET] - Post Detail page - Successful Calling")
+		@Test
+		public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+				// Given
+
+				// When & Then
+				mvc.perform(get("/articles/1"))
+								.andExpect(status().isOk())
+								.andExpect(content().contentType(MediaType.TEXT_HTML))
+								.andExpect(model().attributeExists("article"));
+		}
+
+		// View Get method => Post Search
+		@DisplayName("View[GET] - Post Search Only Page - Successful Calling")
+		@Test
+		public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+				// Given
+
+				// When & Then
+				mvc.perform(get("/articles/search"))
+								.andExpect(status().isOk())
+								.andExpect(content().contentType(MediaType.TEXT_HTML));
+		}
+
+		// View Get method => Post Hashtag Search
+		@DisplayName("View[GET] - Post HashTag Search Page - Successful Calling")
+		@Test
+		public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+				// Given
+
+				// When & Then
+				mvc.perform(get("/articles/search-hashtag"))
+								.andExpect(status().isOk())
+								.andExpect(content().contentType(MediaType.TEXT_HTML));
+		}
 }
