fc88202a883d2fa2a99fe3c204af6d2ec0575d7a
trte135 <trte135@naver.com>
2023-04-06 15:37:33 +0900

    #16 - Board View Basic 틀 Write
    Controller Handler Method로 Test를 통과하게 열어줌.
    View 적절히 틀 잡음.
    Test에서는 media type 검사에서 부가적인 option이 들어갈 수 있음을 확인했기에
    적절히 Test Method 수정함.

--- board-project/src/test/java/com/board/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/board/controller/ArticleControllerTest.java
@@ -27,17 +27,23 @@ class ArticleControllerTest {
 		}
 
 		// View Get method => Post List
-		@Disabled("구현 중")
+//		@Disabled("구현 중")
 		@DisplayName("View[GET] - Post List (Board) page - Successful Calling")
 		@Test
 		public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
 				// Given
 
 				// When & Then => View 로 이름을 붙여줘서 어떤 의미로 Test 하는 건지 알려줌
+				// 여기서 문제는? ContentType method 인데, 이유가 현재 설정한 index.html 에서 charset=utf-8 까지의 가변길이까지 현재 Method 에서는 지원되지 않기 때문에, method 를 ContentTypeCompatibleWith 로 이용하여 호환성에 문제 없도록 변경해줌!(즉, 자기가  컴파일러를 해준다는 소리임)
+				// get 요청을 하는데? /articles 로부터!
 				mvc.perform(get("/articles"))
+								// 요청한 /articles 에서 Ok 싸인을 주고!
 								.andExpect(status().isOk())
-								.andExpect(content().contentType(MediaType.TEXT_HTML))
+								// 요청한 /articles 가 Text_Html Type 으로 보여주게 만들고?
+								.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+								// 요청한 /articles 에서 해당되는 경로의 파일을 불러오고!(or 있어야하고!)
 								.andExpect(view().name("articles/index"))
+								// 요청한 /articles 에서 해당되는 articles 의 Data 를 넘겨줌!
 								.andExpect(model().attributeExists("articles"));
 		}
 
